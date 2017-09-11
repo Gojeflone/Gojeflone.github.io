8fe0c517b068c1c76a2ea2d9af7e56093fab7cd6
Gojeflone <avak1908@gmail.com>
2017-09-11 11:24:30 -0400

    Mail Update
    Comment section should work

---
+++ index.php
@@ -0,0 +1,14 @@
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+$to = 'jgouveia@udel.edu';
+
+$body = "From: $name\n E-mail: $email\n Message: \n $message";
+
+if($_POST['submit']) {
+	if(mail ($to,$subject,$body,$email,$message)) {
+	echo '<p>Your message has been sent!</p>';
+} else {
+	echo '<p>You're missing something! Try again </p>';
+}
+}
