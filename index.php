dadad14b8cbf9baf8ca1c81c1e1ab18dca532bf7
Gojeflone <avak1908@gmail.com>
2017-09-11 11:57:58 -0400

    Bug fix

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $to = 'jgouveia@udel.edu';
 $body = "From: $name\n E-mail: $email\n Message: \n $message";
 
 if($_POST['submit']) {
-	if(mail ($to,$subject,$body,$name)) {
+	if(mail ($to,$subject,$body)) {
 	echo '<p>Your message has been sent!</p>';
 } else {
 	echo '<p>You are missing something! Try again </p>';
