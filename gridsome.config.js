7b0141e2218fae7894ed1c4c1f545c9df529bdc4
Jeremy Gouveia <avak1908@gmail.com>
2020-08-06 11:46:03 -0400

    Added sass-loader, node-sass, style-resources-loader. Added boilerplate code for scss compiling to gridsome config and main.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -4,7 +4,37 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
+// style-resources-loader code
+const path = require('path')
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/assets/sass/_globals.sass'),
+        // or if you use scss
+        path.resolve(__dirname, './src/assets/sass/_globals.scss'),
+        // you can also use a glob if you'd prefer
+        path.resolve(__dirname, './src/assets/sass/*.sass'),
+        // or scss
+        path.resolve(__dirname, './src/assets/sass/*.scss'),
+      ],
+    })
+}
+
 module.exports = {
+  chainWebpack (config) {
+    // Load variables for all vue-files
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+
+    // or if you use scss
+    types.forEach(type => {
+      addStyleResource(config.module.rule('scss').oneOf(type))
+    })
+  }, 
+ 
+
   siteName: 'Gridsome',
   siteUrl: 'https://gojeflone.github.io',
   pathPrefix: '/Gojeflone.github.io',
