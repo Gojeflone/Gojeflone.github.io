74d176d302aba502b3efa50e6bcca71bfdf60ce7
Jeremy Gouveia <avak1908@gmail.com>
2020-08-17 23:59:07 -0400

    Created Projects page

--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -9,11 +9,54 @@
 <template>
 <Layout>
     <div class="Projects container-fluid page-container">
-        <div class="row">
-            <div class="col-12">
-                <h1>These are projects</h1>
-            </div>
-        </div>        
+        <b-row>
+            <b-col cols="12">
+                <h1 class="projects-heading">Projects</h1>
+            </b-col> 
+        </b-row>
+        <b-row class="projects-content">
+            <b-col cols="12">
+                <b-row class="align-items-center">
+                    <b-col cols="12" lg="6" order="2" order-lg="1">
+                        <h4>Deep Roots Horticulture</h4>
+                        <h3>Dec 2019 - Present</h3>
+                        <br/>
+                        <p>
+                            Normally, the work of a developer stops at the engineering of the site. That could not be farther from the reality of this project.
+                            Due to the requirements of the project and the knowledgability of the client, I managed the entire site launch myself. Not to toot my own horn
+                            but it was definitely a great and rewarding experience. The client runs a small business offering landscaping and horticulture services to the areas nearby. 
+                            In the off season, the client was looking for ways to ensure consistent work without having to rely on canvassing the local area. 
+                            <br/>
+                            <br/>
+                            Given the time period for this project, COVID ended up hitting us in the middle of site development. Having such a major event occur during the development
+                            process added another level of complexity that would have otherwise not have been preferred. Needless to say, we were able to launch successfully and establish a great 
+                            and long term relationship with the client. 
+                            <br/>
+                            <br/>
+                            Working on this project gave me experience with Project Management, Front End and Backend development, as well experience with working with the Squarespace environment. 
+                            In my youth, I spent some time making some Wix sites when flash was all the rage. Due to the business requirements of the client and my curiosity for this "Wix"-eque CMS,
+                            I lept at the opportunity. To my surprise, it was nothing like Wix. The process felt more intuitive from a Developer's point of view, and developing custom sections and code 
+                            was incredibly easy. 
+                            <br/>
+                            <br/>
+                            Finally, under Kismet Creative, I assist the client in a marketing capacity, creating marketing materials, collecting mailing lists, running campaigns, etc. It has been an 
+                            interesting journey to say the least, but our methodology at Kismet made execution of this facet of the client's needs very easy to fulfill. 
+                        </p>
+                    </b-col>
+                    <b-col cols="12" lg="6" order="1" order-lg="2">
+                        <div class="project-image-wrapper position-relative">
+                            <a href="https://www.deeprootshorticulture.com/" class="position-absolute h-100 w-100" target="_blank"></a>
+                            <g-image src="~/assets/images/projects/dpr_home_page.png" class="img-fluid"></g-image>
+                            <p class="p-3 text-center">Deep Roots Horticulture is a landscaping and horticulture company that really cares about making the customer feel they're getting quality, reliable service.</p>
+                        </div>
+                    </b-col>
+                </b-row>
+                
+                
+                
+
+            </b-col>
+        </b-row>          
     </div>
 </Layout>    
 </template>
@@ -25,5 +68,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
 </style>
