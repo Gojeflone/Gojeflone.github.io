9ad54ea99d16f54c134fb976c4de0318c82652a6
Jeremy Gouveia <avak1908@gmail.com>
2020-08-19 00:01:57 -0400

    Finished About Page

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -9,8 +9,88 @@
 <template>
   <Layout>
     <div class="About container-fluid page-container">
-      <h1>About us</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+      <b-row>
+        <b-col cols="12"> 
+          <h1>About Me</h1>
+        </b-col>
+      </b-row>
+      <b-row class="about-content pb-4">
+        <b-col cols="12">
+          <div class="section-title mt-4 mb-4">
+            <h4>Polymath Hopeful</h4>
+          </div>          
+          <p>I used to read about the famous polymaths: Shen Kuo, Ben Franklin, Da Vinci and plan to join their numbers. By no means do I claim to be there yet,
+            but I intend to get there step-by-step.
+          </p>
+          <br/>
+          <h5 class="mb-4">The Gameplan</h5>
+          <h6 class="mb-3">Step One: Gain experience in the programming industry - IN PROGRESS </h6>
+          <p>This step is well underway. Having completed four years at the University of Delaware and getting my degree in a Bachelor of Science, I was fortunate to
+            land a great opportunity at Engine Room Technology. At ER, I have spent my time working on developing and designing architecture for web applications. 
+            <br/>
+            <br/>
+            I chose 
+            this job for a number of reasons but I will give you two: first, that although I have a high-level understanding of Computer Science, I lack experience in the practical
+            application of it and second, that I am not familiar with the software engineering industry. It seems natural to me that a wannabe sea captain should avoid unfamiliar waters, and if he so chooses to expand
+            his area of expertise, he should familiarize himself with the new oceans before embarking on the journey. The captain would accept some station on another man's ship and during that time
+            he would glean and discern all that he might need in order to assist the coming journey. 
+            By this same vein, I wish to travel to new terrain, but before I do so I must learn of the treacherous waters and how to navigate. 
+            <br/>
+            <br/>
+            The consultant nature of the business suits me spectacularly. We have an excellent team of Project Managers so direct communication with the client is not required, but encouraged. We have a diverse number of 
+            developers, each with their own social preferences. Unfortunately, I was cursed with the gift of gab, combine that with my years of experience in retail and what you get is a killer combination: a programmer 
+            who can sell his own code. "He could sell hot chocolate to a snowman", my old coworkers at Staples used to tell me. Such a talent must be used for good. 
+            <br/>
+            <br/>
+            Thankfully, at Engine Room, we are encouraged to sell solutions, not products. What's more, at Engine Room we are nudged to think outside the box. Combine this with the customer-facing aspect and 
+            what you get is a great opportunity to learn the industry as well as those tangentially related to it through consulting work. I look forward to continuing to spend my time learning more at Engine Room. 
+          </p>
+        </b-col>
+        <b-col cols="12" class="mt-4">
+          <h6 class="mb-4">Step Two: Start and Build a Company - IN PROGRESS </h6>
+          <p>I founded Kismet Creative LLC in December of 2019 (which was a really dumb decision because if I had waited a month, I would not have had to pay taxes for my company for that year). Kismet specializes in 
+            building brands. Using my programming background, I have been able to use my free time to keep track of new technologies to enable better marketing. I have a penchant for aesthetic and I have to say I have found many lacking. 
+            That said, it's not as if I know the bon-a-fide way of establishing a brand but I aim to succeed and to disrupt the norms of what it means to be a company and what it means to have a brand.
+            <br/>
+            <br/>
+            For the year of 2020, Kismet Creative LLC will continue to build out its customers - currently, we have one - and explore creative projects to get our name out there. We look forward to leading the way on opportunities in the industry, both big and small.  
+          </p>
+        </b-col>
+        <b-col cols="12" class="mt-4">
+          <h6 class="mb-4">Step Three: Learn to Think - IN PROGRESS </h6>
+          <p>
+            Suppose one has an assorted collection of rocks, all roughly the same size and the same weight. Suppose one were to skip one of these rocks down a lake. The rock would jump and hop and skip along the surface of the water, sending ripples throughout the lake.
+            These rocks are all identical by all relavant metrics yet they all each have their distinguished, unique faces. 
+            <br/>
+            <br/>
+            There are some with dark tones and others with light ones. Some with sharp points as if compressed under the Earth's crust to stab you. Some with rough edges, 
+            others with nothing but smooth surfaces - you could almost swear it was a countertop.
+            Yet despite all their differences, each one more different than the last, they ripple about the same and at the end of their journey they all fall down into the depths to ripple no more.
+            <br/>
+            <br/>
+            Mapping this metaphor onto reality, I ponder on what each aspect might be. First there is the stone, their meaning obvious. The stone is me, but then there is the arm, and the person throwing it. I ask myself, "By what means do I wish to thrust myself through society?". 
+            What will be my impetus? My Raison D'être? My passion? That entrancing, embittering companion that soothes and tears the soul like no else. She rips my heart in half and pulls it back together with the wave of her hand. 
+            <br/>
+            <br/>
+            Now, we can sit here and say that any reason I choose in particular is subjective, but, my friend, to that I say, the proof is in the pudding. Surely, the reason may be subjective, but the ripples demonstrate the proper courses. And so, such is the reason for step three. 
+            If I cannot think, then I cannot aim, I cannot throw, and I will certainly make no ripple. To that end, I am reading a book a month, spending time listening to podcasts, practicing ZaZen Meditation, and journaling. 
+          </p>
+        </b-col>
+        <b-col cols="12" class="mt-4">
+          <h6 class="mb-4">Step Four: Learn to Speak - IN PROGRESS </h6>
+          <p>
+
+          </p>
+        </b-col>
+        <b-col cols="12" class="mt-4">
+          <h6 class="mb-4">Step Five: Learn to Listen - IN PROGRESS </h6>
+        </b-col>
+        <b-col cols="12" class="mt-4">
+          <h6 class="mb-4">Step Six: Write a Book </h6>
+        </b-col>       
+      </b-row>
+      
+      
     </div>    
   </Layout>
 </template>
